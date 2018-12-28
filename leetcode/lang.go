ad52dd4a57db0341dc80cde00cf3fe38c9d36644
invzhi <invzhi@protonmail.com>
2018-12-28 19:39:58 +0800

    leetcode: add func Valid for type Lang

--- leetcode/lang.go
+++ leetcode/lang.go
@@ -3,7 +3,7 @@ package leetcode
 // Lang resprents a programming language on leetcode.
 type Lang string
 
-// programming languages
+// programming languages supported on leetcode.
 const (
 	C          Lang = "c"
 	Cpp        Lang = "cpp"
@@ -13,9 +13,27 @@ const (
 	Scala      Lang = "scala"
 	Python     Lang = "python"
 	Python3    Lang = "python3"
-	JavaScript Lang = "javascript"
 	Ruby       Lang = "ruby"
+	JavaScript Lang = "javascript"
 	Swift      Lang = "swift"
 	Go         Lang = "golang"
 	Rust       Lang = "rust"
 )
+
+// Valid check if Lang is supported on leetcode.
+func (l Lang) Valid() bool {
+	switch l {
+	case C, Cpp, CSharp:
+		return true
+	case Java, Kotlin, Scala:
+		return true
+	case Python, Python3, Ruby:
+		return true
+	case JavaScript, Swift:
+		return true
+	case Go, Rust:
+		return true
+	default:
+		return false
+	}
+}
